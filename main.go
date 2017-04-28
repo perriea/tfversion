9e829c3841609380d925d7abd79288731409d9d2
Aurélien Perrier <a.perrier89@gmail.com>
2017-04-28 23:47:59 +0200

    + Add func install

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
   "github.com/perriea/tfversion/terraform/download"
+  "github.com/perriea/tfversion/terraform/install"
 )
 
 func main()  {
   tfdownload.Run()
+  tfinstall.Run()
 }
