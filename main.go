f862e30a83a1c1f5f8f912eb219d81293a253bd6
Aurélien Perrier <a.perrier89@gmail.com>
2017-04-30 15:50:47 +0200

    ~ Modify gestion errors, add commentaries

--- main.go
+++ main.go
@@ -2,24 +2,34 @@ package main
 
 import (
     "flag"
+    "errors"
 
+    "github.com/fatih/color"
     "github.com/perriea/tfversion/terraform/download"
     "github.com/perriea/tfversion/terraform/install"
     "github.com/perriea/tfversion/terraform/list"
-    "github.com/fatih/color"
 )
 
 var (
-    version string
-    list    bool
+    // Errors
+    err_msg string
     check   bool
+    err     error
     fatal   *color.Color
+    // Flag func version param
+    version string
+    // Flag launch func List
+    list    bool
 )
 
 func init()  {
     check = false
-    fatal = color.New(color.FgRed, color.Bold)
 
+    // Init show error message
+    fatal = color.New(color.FgRed, color.Bold)
+    err_msg = "[ERROR] Too many argument or nothing !"
+
+    // Flags CLI
     flag.BoolVar(&list, "list", false, "List version of terraform")
     flag.StringVar(&version, "version", "0", "Version of terraform to install or switch")
     flag.Parse()
@@ -28,13 +38,19 @@ func init()  {
 func main()  {
 
     if list == true && version == "0" {
+        // Show version terraform
         tflist.Run()
     } else if list == false && version != "0" {
+        // Lauch Terraform download
         check = tfdownload.Run(version)
+        // Check if download is done and install
         if check {
           tfinstall.Run(version)
         }
     } else {
-        fatal.Println("[ERROR] Too many argument or nothing !")
+        err = errors.New(err_msg)
+        if err != nil {
+          fatal.Println(err)
+        }
     }
 }
