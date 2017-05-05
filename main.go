3ad3fd9225a2a3393ffb3ca8c9e5c41c86d0d4bb
Aurélien Perrier <a.perrier89@gmail.com>
2017-05-06 01:59:28 +0200

    ~ Change variable name (norme Golang)

--- main.go
+++ main.go
@@ -1,87 +1,87 @@
 package main
 
 import (
-    "flag"
+	"flag"
 
-    "github.com/perriea/tfversion/system/network"
-    "github.com/perriea/tfversion/terraform/download"
-    "github.com/perriea/tfversion/terraform/install"
-    "github.com/perriea/tfversion/terraform/list"
-    "github.com/perriea/tfversion/system/files"
-    "github.com/perriea/tfversion/error"
+	"github.com/perriea/tfversion/error"
+	"github.com/perriea/tfversion/system/files"
+	"github.com/perriea/tfversion/system/network"
+	"github.com/perriea/tfversion/terraform/download"
+	"github.com/perriea/tfversion/terraform/install"
+	"github.com/perriea/tfversion/terraform/list"
 )
 
 var (
-    // Errors
-    err_network       bool
-    check             bool
-    err               error
-    // Flag func version param
-    path_bin          string
-    path_tmp          string
-    install           string
-    // Flag launch func List
-    list_online       bool
-    list_offline      bool
-    cleanup           bool
+	// Errors
+	errNetwork bool
+	check      bool
+	err        error
+	// Flag func version param
+	pathBin string
+	pathTmp string
+	install string
+	// Flag launch func List
+	listOnline  bool
+	listOffline bool
+	cleanup     bool
 )
 
-func init()  {
-    // Simply error
-    check = false
-    err_network = false
-    // Paths
-    path_bin = "/terraform/bin/"
-    path_tmp = "/terraform/tmp/"
-    // Flags CLI
-    flag.BoolVar(&list_online, "liston", false, "List online version of terraform")
-    flag.BoolVar(&list_offline, "listoff", false, "List local version of terraform")
-    flag.BoolVar(&cleanup, "cleanup", false, "Clean cache (tmp files)")
-    flag.StringVar(&install, "install", "0", "Version of terraform to install or switch")
-    flag.Parse()
+func init() {
+	// Simply error
+	check = false
+	errNetwork = false
+	// Paths
+	pathBin = "/terraform/bin/"
+	pathTmp = "/terraform/tmp/"
+	// Flags CLI
+	flag.BoolVar(&listOnline, "liston", false, "List online version of terraform")
+	flag.BoolVar(&listOffline, "listoff", false, "List local version of terraform")
+	flag.BoolVar(&cleanup, "cleanup", false, "Clean cache (tmp files)")
+	flag.StringVar(&install, "install", "0", "Version of terraform to install or switch")
+	flag.Parse()
 }
 
-func main()  {
-    // Check if internet is available (releases.hashicorp.com)
-    err_network = tfnetwork.Run()
-    if list_online == true {
+func main() {
+	// Check if internet is available (releases.hashicorp.com)
+	errNetwork = tfnetwork.Run()
+	if listOnline == true {
 
-        if err_network {
-            // Show version terraform
-            tflist.ListOn()
-        } else {
-            // No network
-            tferror.Run(2, "[ERROR] No internet connection ...")
-        }
+		if errNetwork {
+			// Show version terraform
+			tflist.ListOn()
+		} else {
+			// No network
+			tferror.Run(2, "[ERROR] No internet connection ...")
+		}
 
-    } else if list_offline == true {
-        // List all versions local
-        tflist.ListOff()
+	} else if listOffline == true {
+		// List all versions local
+		tflist.ListOff()
 
-    } else if install != "0" {
+	} else if install != "0" {
 
-        if err_network {
-            // Lauch Terraform download
-            tffiles.CreateFolder(path_tmp, 0755)
-            check = tfdownload.Run(install)
+		if errNetwork {
+			// Lauch Terraform download
+			tffiles.CreateFolder(pathTmp, 0755)
+			check = tfdownload.Run(install)
 
-            // Check if download is done and install
-            if check {
-                tffiles.CreateFolder(path_bin, 0755)
-                tfinstall.Run(install)
-            }
+			// Check if download is done and install
+			if check {
+				tffiles.CreateFolder(pathBin, 0755)
+				tfinstall.Run(install)
+			}
 
-        } else {
-            // No network
-            tferror.Run(2, "[WARN] No internet connection ...")
-        }
+		} else {
+			// No network
+			tferror.Run(2, "[WARN] No internet connection ...")
+		}
 
-    } else if cleanup {
-        // Delete all cache
-        tflist.Cleanup()
+	} else if cleanup {
+		// Delete all cache
+		tflist.Cleanup()
 
-    } else {
-        // Show version
-        ShowVersion()
-    }
+	} else {
+		// Show version
+		ShowVersion()
+	}
 }
--- terraform/install/install.go
+++ terraform/install/install.go
@@ -1,34 +1,34 @@
 package tfinstall
 
 import (
-  "os/user"
-  "fmt"
+	"fmt"
+	"os/user"
 
-  "github.com/perriea/tfversion/error"
-  "github.com/perriea/tfversion/system/files"
+	"github.com/perriea/tfversion/error"
+	"github.com/perriea/tfversion/system/files"
 )
 
 var (
-    path_bin  string
-    path_zip  string
-    usr       *user.User
-    err       error
+	pathBin  string
+	pathZip string
+	usr      *user.User
+	err      error
 )
 
-func init()  {
-    usr, err = user.Current()
-    tferror.Panic(err)
+func init() {
+	usr, err = user.Current()
+	tferror.Panic(err)
 
-    path_zip = usr.HomeDir + "/terraform/tmp/terraform-%s.zip"
-    path_bin = usr.HomeDir + "/terraform/bin/"
+	pathZip = usr.HomeDir + "/terraform/tmp/terraform-%s.zip"
+	pathBin = usr.HomeDir + "/terraform/bin/"
 }
 
-func Run(version string)  {
+func Run(version string) {
 
-    // Unzip zip archive
-    fmt.Printf("Unzip file ...\n")
-    tffiles.UnZip(fmt.Sprintf(path_zip, version), path_bin)
-    fmt.Printf("Install the binary file ...\n")
+	// Unzip zip archive
+	fmt.Printf("Unzip file ...\n")
+	tffiles.UnZip(fmt.Sprintf(pathZip, version), pathBin)
+	fmt.Printf("Install the binary file ...\n")
 
-    tferror.Run(1, fmt.Sprintf("Installed %s, Thanks ! ♥\n", version))
+	tferror.Run(1, fmt.Sprintf("Installed %s, Thanks ! ♥\n", version))
 }
