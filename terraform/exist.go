423d601518002aefbd5067f6c2c4fae1472e6b2b
Aurélien PERRIER <a.perrier89@gmail.com>
2018-12-12 15:44:04 +0100

    Adding methods Existing release

---
+++ terraform/exist.go
@@ -0,0 +1,49 @@
+package terraform
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"path/filepath"
+	"regexp"
+	"runtime"
+)
+
+// Regex version submited
+func (release Release) Regex() bool {
+	match, err := regexp.MatchString("[0-9]+\\.[0-9]+\\.[0-9]+(-(rc|beta)[0-9]+)?", release.Version)
+	if err != nil {
+		return false
+	}
+
+	return match
+}
+
+// LocalExist version zipped offline
+func (release Release) LocalExist() bool {
+	if _, err := os.Stat(filepath.Join(release.Home, folders["tmp"], fmt.Sprintf("terraform-%s.zip", release.Version))); !os.IsNotExist(err) {
+		fmt.Println("Already in cache ...")
+		return true
+	}
+
+	return false
+}
+
+// RemoteExist version zipped online
+func (release Release) RemoteExist() bool {
+	callurl := fmt.Sprintf(release.Repository, release.Version, release.Version, runtime.GOOS, runtime.GOARCH)
+	fmt.Println(callurl)
+	resp, err := release.HTTPclient.Get("https://" + callurl)
+	if err != nil {
+		fmt.Println(err)
+		return false
+	}
+	defer resp.Body.Close()
+
+	// Verify code equal 200
+	if (err == nil) && (resp.StatusCode == http.StatusOK) {
+		return true
+	}
+
+	return false
+}
