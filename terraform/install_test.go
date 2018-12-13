1d22daaf7937b96e421a4be076fc40e9accf1a37
Aurélien PERRIER <a.perrier89@gmail.com>
2018-12-13 17:31:14 +0100

    Fixing Go tests

--- terraform/install_test.go
+++ terraform/install_test.go
@@ -1,42 +1,73 @@
 package terraform
 
 import (
+	"crypto/tls"
 	"fmt"
+	"net/http"
 	"testing"
+
+	homedir "github.com/mitchellh/go-homedir"
 )
 
-// TestInstall : testing installation
-func TestInstall(t *testing.T) {
+func TestDownload(t *testing.T) {
 	var (
+		home     string
 		versions []string
+		err      error
 	)
 
 	versions = []string{"0.11.0", "0.11.0-beta1", "0.11.0-rc1", "0.10.8", "0.7.2", "0.1.0"}
 
+	home, err = homedir.Dir()
+	if err != nil {
+		panic(err)
+	}
+
 	for _, version := range versions {
-		err = Install(version, true)
+		release := Release{
+			Home:       home,
+			Version:    version,
+			HTTPclient: &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}},
+			Repository: "releases.hashicorp.com/terraform/%s/terraform_%s_%s_%s.zip",
+		}
+
+		err = release.Download(true)
 		if err != nil {
-			t.Fatalf("installation failed (%s)\n", version)
+			t.Fatalf("download failed (%s)\n", version)
 		} else {
-			fmt.Printf("installation OK (%s)\n", version)
+			t.Logf("download OK (%s)\n", version)
 		}
 	}
 }
 
-// TestUnInstall : testing installation
-func TestUnInstall(t *testing.T) {
+// TestInstall : testing installation
+func TestInstall(t *testing.T) {
 	var (
+		home     string
 		versions []string
+		err      error
 	)
 
-	versions = []string{"0.11.0", "0.11.0-beta1", "0.11.0-rc1", "0.10.8", "0.7.2", "0.1.0", "!", "all"}
+	versions = []string{"0.11.0", "0.11.0-beta1", "0.11.0-rc1", "0.10.8", "0.7.2", "0.1.0"}
+
+	home, err = homedir.Dir()
+	if err != nil {
+		panic(err)
+	}
 
 	for _, version := range versions {
-		err = UnInstall(version, true)
+		release := Release{
+			Home:       home,
+			Version:    version,
+			HTTPclient: &http.Client{Transport: &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}},
+			Repository: "releases.hashicorp.com/terraform/%s/terraform_%s_%s_%s.zip",
+		}
+
+		err = release.Install(true)
 		if err != nil {
-			t.Fatalf("uninstall failed (%s)\n", version)
+			t.Fatalf("installation failed (%s)\n", version)
 		} else {
-			fmt.Printf("uninstall OK (%s)\n", version)
+			fmt.Printf("installation OK (%s)\n", version)
 		}
 	}
 }
